9998966116fe1e1c14f71b842f88bf952bef0a91
Oliver Schneider <oliverschneider89+sweetpi@gmail.com>
2014-01-05 21:00:24 +0100

    added damonized mode

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
-require('coffee-script');
-require('./startup');
+var startStopDaemon = require('start-stop-daemon');
+var path = require('path');
+
+logFile = path.resolve(__dirname, '../../pimatic-daemon.log');
+var options = {
+  logFile: logFile,
+  outFile: logFile,
+  errFile: logFile,
+  cwd: __dirname,
+  env: { 'PIMATIC_DAEMONIZED': true },
+  max: 3 //the script will run 3 times at most
+};
+
+startStopDaemon(options, function() {
+  require('coffee-script');
+  require('./startup');
+});
